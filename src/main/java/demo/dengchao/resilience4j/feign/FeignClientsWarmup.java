d45e218b3ce44d76aa915e34869ae9b4372aadd5
dengchao <2325690622@qq.com>
2022-01-08 15:52:48 +0800

    refactor: extract FeignClientsWarmup from Resilience4jFeignApplication

---
+++ src/main/java/demo/dengchao/resilience4j/feign/FeignClientsWarmup.java
@@ -0,0 +1,42 @@
+package demo.dengchao.resilience4j.feign;
+
+import demo.dengchao.resilience4j.feign.broken.BrokenHttpBinService;
+import demo.dengchao.resilience4j.feign.broken.ExtendedBrokenHttpBinService;
+import demo.dengchao.resilience4j.feign.works.ExtendedWorksHttpBinService;
+import demo.dengchao.resilience4j.feign.works.WorksHttpBinService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class FeignClientsWarmup {
+
+    private final WorksHttpBinService works;
+    private final BrokenHttpBinService broken;
+    private final ExtendedWorksHttpBinService extendWorks;
+    private final ExtendedBrokenHttpBinService extendBroken;
+
+    @Getter
+    private boolean success = true;
+
+    @PostConstruct
+    private void postConstruct() {
+        works.ip();
+        extendWorks.ip();
+
+        if (broken.ip().getOrigin().equals("localhost")) {
+            log.error("Oh no, Resilience4j time limiter fallback triggered even not timeout.");
+            success = false;
+        }
+        if (extendBroken.ip().getOrigin().equals("localhost")) {
+            log.error("Oh no, Resilience4j time limiter fallback triggered even not timeout.");
+            success = false;
+        }
+    }
+
+}
